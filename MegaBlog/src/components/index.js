17fb400bf88de84efc461482e5c019d2e3bd0507
Harsh sheladiya <shv245245@gmail.com>
2024-02-15 18:53:47 +0530

    megablog

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    Logoutbtn
+    Logoutbtn,
+    RTE
 }
